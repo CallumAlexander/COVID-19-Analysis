19d018d2d719fcf6614a25db7da548346aec3208
Cal Alexander <s1931801@ed.ac.uk>
2020-03-30 12:31:24 +0100

    Imports data\n filters to italy and plots the number of cases

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# -*- This file is Property of Callum 'Cal' Alexander -*-
+# -*- CONTACT INFORMATION -*-
+# -*- Email : s1931801@ed.ac.uk -*-
+# -*- Instagram : cal.zander -*-
+# -*- Twitter : calzander -*-
+
+"""
+Created on Thu Mar 26 17:58:00 2020
+@author: cal
+"""
+
